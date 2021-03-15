ab1637812f4956d734508da8fd272b4a0564a398
Chris Williams <diodesign@tuta.io>
2021-03-15 02:10:40 -0700

    Support unaligned u32 reads

--- asm/utils.s
+++ asm/utils.s
@@ -53,21 +53,50 @@ platform_set_supervisor_return:
 # => a0 = address to read
 # <= a0 = u32 read in
 platform_read_u32_as_prev_mode:
-  csrrs t1, mstatus, x0           # keep a copy of mstatus in case the read faults
-  la    t2, trap_read_u32_fault   # t2 = address of our temporary fault catcher
-  csrrw t2, mtvec, t2             # swap t2 and original fault handler
+  csrrs t5, mstatus, x0           # t5 = mstatus in case the read faults
+  la    t6, trap_read_u32_fault   # t6 = address of our temporary fault catcher
+  csrrw t6, mtvec, t6             # swap t6 and original fault handler
 
   li    t0, (1 << 17) | (1 << 19) # set bits 17 (MPRV) and 19 (MXR) to read as previous mode
   csrrs x0, mstatus, t0
-  lw    a0, (a0)                  # do the read
-  csrrc x0, mstatus, t0           # clear those MPRV and MXR bits
 
-  csrrw t2, mtvec, t2             # restore the original fault handler
+  # some hardware can't do unaligned reads, so if we're not 32-bit aligned
+  # read the instruction byte by byte instead
+  andi  t0, a0, 3
+  beq   x0, t0, platform_read_u32_as_prev_mode_aligned
+  
+  # read the unaligned instruction a byte at a time
+  mv    t0, x0                    # where we'll store our fetched instruction
+  li    t1, 4                     # loop counter: copy 4 bytes
+platform_read_u32_as_prev_mode_byte_loop:
+  slli  t0, t0, 8                 # shift u32 register up to make space for next byte
+  addi  t1, t1, -1                # decrement loop counter now so we copy bytes 3 to 0 inclusive
+  add   t2, t1, a0                # compute address of byte to fetch
+  lbu   t2, (t2)                  # read in the byte without sign extending
+  or    t0, t0, t2                # paste the byte's bits onto low byte of u32 register
+  bne   x0, t1, platform_read_u32_as_prev_mode_byte_loop
+  mv    a0, t0                    # fetched 32-bit word expected in a0
+
+  # sign extend a0 if necessary to keep rust happy
+  # TODO: not sure why this is neccesary. some FFI issue?
+  li    t0, 1 << 31
+  and   t0, t0, a0
+  beq   x0, t0, platform_read_u32_as_prev_mode_done
+  li    t0, 0xffffffff00000000
+  or    a0, a0, t0
+  j     platform_read_u32_as_prev_mode_done
+
+platform_read_u32_as_prev_mode_aligned:
+  lw   a0, (a0)                   # do the 32-bit aligned read into a0
+
+platform_read_u32_as_prev_mode_done:
+  csrrw x0, mstatus, t5           # restore previous status
+  csrrw x0, mtvec, t6             # restore the original fault handler
   ret
 
 # pin the blame on the previous mode (in mstatus.mpp) by swapping in previous mstatus
 # this means the fault will appear from that mode
 trap_read_u32_fault:
-  csrrw x0, mstatus, t1           # restore the mstatus with the previous mpp, and no MPRV + MXR set
-  csrrw x0, mtvec, t2             # restore the original fault handler
-  jalr  x0, t2                    # jump to fault handler to deal with error
+  csrrw x0, mstatus, t5           # restore the mstatus with the previous mpp, and no MPRV + MXR set
+  csrrw x0, mtvec, t6             # restore the original fault handler
+  jalr  x0, t6                    # jump to fault handler to deal with error
