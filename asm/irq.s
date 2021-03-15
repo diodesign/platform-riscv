84d3309feb2cdf590ff1c04d96440acb323ced7d
Chris Williams <diodesign@tuta.io>
2021-03-15 02:11:26 -0700

    Don't delegate to non-S-mode CPUs * Also include early trap debug code for SiFive SoCs

--- asm/irq.s
+++ asm/irq.s
@@ -24,6 +24,12 @@ irq_early_init:
   la    t0, machine_irq_handler
   csrrw x0, mtvec, t0
 
+  # skip delegations if there's no supervisor mode to delegate to
+  csrrs     t0, misa, x0
+  li        t1, 1 << 18     # bit 18 set in misa = S mode present
+  and       t0, t0, t1      # if it's not set, no S mode, so park the core
+  beq       x0, t0, irq_early_init_post_delegation
+
   # delegate most exceptions to the supervisor capsule
   # so that it can deal with them direct. for a given exception,
   # bit = 1 to delegate, 0 = pass to the machine-level hypervisor 
@@ -47,6 +53,7 @@ irq_early_init:
   li    t0, 0x333
   csrrw x0, mideleg, t0
 
+irq_early_init_post_delegation:
   # enable all interrupts: set bit 3 in mstatus to enable machine irqs (MIE)
   # to receive hardware interrupts and exceptions
   csrrsi x0, mstatus, 1 << 3
@@ -63,6 +70,7 @@ irq_early_init:
   # lw  x\reg, (\reg * 4)(sp) # RV32
   ld  x\reg, (\reg * 8)(sp)
 .endm
+
 
 .align 8
 # Entry point for machine-level handler of interrupts and exceptions
@@ -126,3 +134,87 @@ continue:
   # finally, restore the interrupted code's sp and return
   PULL_REG 2
   mret
+
+
+.align 8
+# use this interrupt handler to debug failures during system bring-up
+# on boards that use SiFive's UART serial controller.
+# this doesn't return, so it's not for normal use. it outputs:
+#
+# I SoC CPU core ID
+# C Cause of the trap
+# E Address of instruction at time of trap
+# A Memory address that triggered a bad access trap
+# R Return address register at time of trap
+# S Stack pointer at time of trap
+#
+sifive_guru_mediation:
+  mv    t6, ra
+
+  call print_newline
+  csrrs t0, mhartid, x0
+  li    t1, 'I'
+  call print_hex
+  csrrs t0, mcause, x0
+  li    t1, 'C'
+  call print_hex
+  csrrs t0, mepc, x0
+  li    t1, 'E'
+  call print_hex
+  csrrs t0, mtval, x0
+  li    t1, 'A'
+  call print_hex
+  mv    t0, t6
+  li    t1, 'R'
+  call print_hex
+  mv    t0, sp
+  li    t1, 'S'
+  call print_hex
+
+halt:
+  j halt
+
+# t0 = value to write, t1 = one character label, scratches t1, t2, t3
+print_hex:
+  addi  sp, sp, -8
+  sd    ra, (sp)
+  call  print_char
+  li    t1, ' '
+  call  print_char
+  la    t3, chars
+  li    t2, 64
+print_hex_loop:
+  addi  t2, t2, -4
+  srl   t1, t0, t2
+  andi  t1, t1, 0xf
+  add   t1, t1, t3
+  lb    t1, (t1)
+  call  print_char
+  bne   x0, t2, print_hex_loop
+  call  print_newline
+  ld    ra, (sp)
+  addi  sp, sp, 8
+  ret
+
+print_newline:
+  addi  sp, sp, -8
+  sd    ra, (sp)
+  li    t1, '\r'
+  call  print_char
+  li    t1, '\n'
+  call  print_char
+  ld    ra, (sp)
+  addi  sp, sp, 8
+  ret
+
+# t1 = character to write, scratches t4, t5
+print_char:
+  li    t4, 0x10010000
+  lw    t4, (t4)
+  bne   x0, t4, print_char
+  li    t4, 0x10010000
+  sw    t1, (t4)
+  ret
+
+chars:
+.ascii "0123456789abcdef"
